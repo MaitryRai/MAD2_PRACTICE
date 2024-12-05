330fc0f6586f015d886ad1d264a7252c7486a1cb
Maitry <maitrysharma5@gmail.com>
2024-12-05 06:09:12 +0530

    MAD2 PROJECT LOGIN-REGISTERATION V1

---
+++ backend/routes.py
@@ -0,0 +1,64 @@
+from flask import current_app as app,request,jsonify,render_template
+from flask_security import auth_required,verify_password,hash_password
+from backend.models import db
+datastore=app.security.datastore
+
+@app.get('/')   
+def home():
+    return render_template("index.html")
+
+@app.get('/protected')
+@auth_required('token')
+def protected():
+    return "<h1>Only accessible by authenticated user</h1>"
+
+
+@app.route('/login',methods=['POST'])
+def login():
+    data=request.get_json()
+    
+    email=data.get('email')
+    password=data.get('password')
+    
+    if not email or not password:
+        return jsonify({"message":"Invalid inputs"}),404
+    
+    user=datastore.find_user(email=email)
+    
+    if not user:
+        return jsonify({"message":"Invalid email"}),404
+    
+    if verify_password(password,user.password):
+        return jsonify({"token":user.get_auth_token(),'email':user.email,'role':user.roles[0].name,'id':user.id})
+    
+    return jsonify({"message":"Wrong password"}),400
+
+
+
+
+@app.route('/register',methods=['POST'])
+def register():
+    data=request.get_json()
+    
+    email=data.get('email')
+    password=data.get('password')
+    role=data.get('role')
+    
+    if not email or not password or not role:
+        return jsonify({"message":"Invalid inputs"}),404
+    
+    
+    user=datastore.find_user(email=email)
+    
+    
+    if user:
+        return jsonify({"message":"user already exists"}),404
+    
+  
+    try:
+        datastore.create_user(email=email,password=hash_password(password),roles=[role])
+        db.session.commit()
+        return jsonify({"message":"user created"}),200
+    except:
+        db.session.rollback()
+        return jsonify({"message":"error creating user"}),400
---
+++ frontend/utils/router.js
@@ -0,0 +1,19 @@
+const Home={
+    template : `<h1> this is home </h1>`
+}
+
+import LoginPage from "../Pages/LoginPage.js";
+import RegisterPage from "../pages/RegisterPage.js"
+
+const routes =[
+    {path:'/',component:Home},
+    {path:'/login',component:LoginPage},
+    {path:"/register",component:RegisterPage},
+];
+
+const router=new VueRouter({
+    routes
+
+})
+
+export default router;
